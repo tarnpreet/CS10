c8b67171d3070b740729f08384339b2156b6009a
1100003023 <1100003023@L815WD220033.edu.cbe.ab.ca>
2022-04-28 15:05:16 -0600

    ++

--- Credit3/src/Assignment1.java
+++ Credit3/src/Assignment1.java
@@ -1,14 +1,30 @@
 
 public class Assignment1{
+	
+	/*
+	Program: ${Assignment1}.java          Date: ${4/28/2022}
+	Author: Tarnpreet Chahal
+	School: CHHS
+	Course: Computer Science 10
+	 
+	*/
 
 public static void main(String args [])
 {
-String main = "From: Mr. Wilson"
-+ " Address: Acer Computer, Lab 314 /r"
-+ " Date: April 4th, 2022"
-+ " To: Tarnpreet Chahal"
-+ " Message: Help! I'm trapped inside a computer!";
+String main = "From: Mr. Wilson\r"
++ " Address: Acer Computer, Lab 314\r"
++ " Date: April 4th, 2022\r"
++ " To: Tarnpreet Chahal\r"
++ " Message: Help! I'm trapped inside a computer!\r";
 
 System.out.println(main);
 }
 }
+
+/* Screen Dump
+ From: Mr. Wilson 
+ Address: Acer Computer, Lab 314
+ Date: April 4th, 2022 
+ To: Tarnpreet Chahal 
+ Message: Help! I'm trapped inside a computer!
+ */
